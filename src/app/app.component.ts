4a36d0cb91e17a312f7c4b14b460d9fc41e23ea4
Prashanth <imprash4u@gmail.com>
2021-11-16 01:36:29 +0530

    Added: Output Screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl = '../assets/images/logo.jpg';
+  url="https://www.bridgelabz.com";
+  userName: string="";
+  nameError: string="";
+
+
+onClick($event){
+  console.log("Save button is clicked",$event);
+  window.open(this.url,"_blank");
+}
+
+onInput($event){
+  console.log("change Event occured!", $event.data);
+  const nameRegexP = RegExp('^[A-Z]{1}[A-Za-z//s]{2,}$');
+  if(nameRegexP.test(this.userName)){
+    this.nameError="";
+    return;
+  }
+  this.nameError ="Name is Incorrect";
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+// importing core modules of angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// whenever cli is used to generate new components services,it 
+// will automatically update this file to immport and add them here so we can see update
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
